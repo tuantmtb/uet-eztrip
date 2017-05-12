d124c4e1bac447df09f6fcd121aa7aac22a0d2a4
Tran Minh Tuan <tuantmtb@gmail.com>
2017-05-12 22:00:03 +0700

    update migrate

--- app/Order.php
+++ app/Order.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    /**
+     * return tour trong hóa đơn
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function tour()
+    {
+        return $this->belongsTo('\App\Tour', 'tour_id');
+    }
+
+
+    /**
+     * return user là Hướng dẫn viên tạo tour đó
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function tourist()
+    {
+        return $this->belongsTo('\App\User', 'tourist_id');
+    }
+
 }
--- app/Tour.php
+++ app/Tour.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tour extends Model
 {
-    //
+    /**
+     * return user là Hướng dẫn viên tạo tour đó
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function tourguide()
+    {
+        return $this->belongsTo('\App\User', 'tourguide_id');
+    }
+
+    /**
+     * return các hóa đơn đã đặt trong tour đó
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function orders()
+    {
+        return $this->hasMany('\App\Order', 'tour_id');
+    }
+
+
 }
