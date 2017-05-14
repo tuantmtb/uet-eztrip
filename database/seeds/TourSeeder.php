a5885106bf13e24557a288bf68f6771986175d19
Tran Minh Tuan <tuantmtb@gmail.com>
2017-05-14 14:56:14 +0700

    update index

--- database/seeds/TourSeeder.php
+++ database/seeds/TourSeeder.php
@@ -13,6 +13,7 @@ class TourSeeder extends Seeder
     public function run()
     {
         for ($i = 0; $i < 9; $i++) {
+            $num = 8 - $i;
             Tour::create([
                 'name' => 'Hanoi Foody Tour - Half day',
                 'description' => 'Hanoi foody tour is the best choice to discover the real ancient Hanoi Old Quarter while tasting amazing authentic street food. Walking through the narrow, chaotic streets of the Old Quarter, you can stop along the way to try some of many local delicacies on offer.',
@@ -21,7 +22,7 @@ class TourSeeder extends Seeder
                 'price' => '22',
                 'time_duration' => 3,
                 'url_cover' => Config::get('app.url') . '/img2/cover/' . $i . '.jpg',
-                'url_gird' => Config::get('app.url') . '/img2/cover/' . (8 - $i) . '.jpg',
+                'url_gird' => Config::get('app.url') . '/img2/cover/' . $num . '.jpg',
                 'tourguide_id' => 4
             ]);
         }
