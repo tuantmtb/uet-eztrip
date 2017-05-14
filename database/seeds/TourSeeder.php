57fca09d3d766d4c21023f1d1bf6b184743e6a48
Tran Minh Tuan <tuantmtb@gmail.com>
2017-05-14 14:08:14 +0700

    update index

--- database/seeds/TourSeeder.php
+++ database/seeds/TourSeeder.php
@@ -12,17 +12,19 @@ class TourSeeder extends Seeder
      */
     public function run()
     {
-        Tour::create([
-            'name' => 'Hanoi Foody Tour - Half day',
-            'description' => 'Hanoi foody tour is the best choice to discover the real ancient Hanoi Old Quarter while tasting amazing authentic street food. Walking through the narrow, chaotic streets of the Old Quarter, you can stop along the way to try some of many local delicacies on offer.',
-            'place' => 'Hanoi',
-            'city' => 'Hanoi',
-            'price' => '22',
-            'time_duration' => 3,
-            'url_cover' => 'https://triip.imgix.net/triips/images/1232/1269/3e54b1da204511e3a792f20cfe3c2acb.jpg?crop=faces&fit=crop',
-            'url_gird' => 'https://triip.imgix.net/triips/images/21386/23160/9f163f8e316711e7bcce4eba7c8cbe05_1920x1080.jpg?dpr=2&q=50&w=490',
-            'tourguide_id' => 4
-        ]);
+        for ($i = 0; $i < 9; $i++) {
+            Tour::create([
+                'name' => 'Hanoi Foody Tour - Half day',
+                'description' => 'Hanoi foody tour is the best choice to discover the real ancient Hanoi Old Quarter while tasting amazing authentic street food. Walking through the narrow, chaotic streets of the Old Quarter, you can stop along the way to try some of many local delicacies on offer.',
+                'place' => 'Hanoi',
+                'city' => 'Hanoi',
+                'price' => '22',
+                'time_duration' => 3,
+                'url_cover' => 'http://localhost:8000/img2/cover/' . $i . '.jpg',
+                'url_gird' => 'http://localhost:8000/img2/cover/' . 8 - $i . '.jpg',
+                'tourguide_id' => 4
+            ]);
+        }
 
     }
 }
